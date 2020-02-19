780e62a76e9eca8b05542adebe152163f9b96abc
rafael gonzalez <rafae11@yahoo.com>
2020-02-19 19:14:58 +0000

    added packages

--- MyUnitTests/MyTargetClassTests.cs
+++ MyUnitTests/MyTargetClassTests.cs
@@ -1,31 +1,37 @@
 ﻿using UnitTestTargetProject;
 using Xunit;
+using NUnit.Framework;
 
 namespace MyUnitTests
 {
+    [TestFixture]
+
     public class MyTargetClassTests
     {
-        [Fact]
+        [Test]
+
         public void VerifySummation()
         {
             const int intFirstNumber = 15;
             const int intsecondNumber = 10;
             var objTargetClass = new MyTargetClass();
             var intResult = objTargetClass.AddNumbers(intFirstNumber, intsecondNumber);
-            Assert.Equal(15 + 10, intResult);
+            NUnit.Framework.Assert.AreEqual(15 + 10, intResult);
         }
 
-        [Fact]
+        [Test]
+
         public void VerifySubtraction()
         {
             const int intFirstNumber = 15;
             const int intsecondNumber = 10;
             var objTargetClass = new MyTargetClass();
             var intResult = objTargetClass.SubractNumbers(intFirstNumber, intsecondNumber);
-            Assert.Equal(15 - 10, intResult);
+            NUnit.Framework.Assert.AreEqual(15 - 10, intResult);
         }
 
-        [Fact]
+        [Test]
+
         public void VerifyDivision()
         {
             const int intFirstNumber = 15;
@@ -34,9 +40,9 @@ namespace MyUnitTests
             
             // in this though there seems to be some data loss.
             var intResult = objTargetClass.DivideNumbers(intFirstNumber, intsecondNumber);
-            
+
             // ensure this is always a int.
-            Assert.Equal((int)(15 / 10), intResult);
+            NUnit.Framework.Assert.AreEqual((int)(15 / 10), intResult);
         }
     }
 }
